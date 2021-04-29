e2945e378a8a03bbc21cf1d66a5763275dd2f40d
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-04-29 22:03:16 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,27 @@
 import React from 'react';
+
 import axios from 'axios';
 
-const Home = () => {
+export default class Home extends React.Component {
+  state = {
+    pokemons: []
+  }
 
-    state = {
-        pokemons: []
-      };
+  componentDidMount() {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+      .then(res => {
+        const pokemons = res.data;
+        console.log(pokemons); //Devuelve un array de objetos
+        this.setState({ pokemons });
+      })
+  }
 
-      componentDidMount() {
-        axios.get(`https://pokeapi.co/api/v2/pokemon/`)
-          .then(res => {
-            const pokemons = res.data;
-            this.setState({ pokemons });
-            console.log(pokemons);
-          })
-      }
-      
+  render() {
     return (
-        <div>
-            <h1> Home</h1>
-        </div>
+      <div>
+        { this.state.pokemons.map(poke =>
+            <p>{poke.name} </p>)}
+      </div>
     )
+  }
 }
-
-export default Home;

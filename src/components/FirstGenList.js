cdcb92e50bda3f7a4554ed42445591cb61bd49ac
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-04-30 16:35:36 +0100

    Update FirstGenList.js

--- src/components/FirstGenList.js
+++ src/components/FirstGenList.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const FirstGenList = () => {
+import PokemonCard from "./PokemonCard";
+
+export default class FirstGenList extends React.Component {
+  state = {
+    url :'https://pokeapi.co/api/v2/pokemon?limit=151',
+    pokemons: null
+  }
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemons : res.data['results']})
+  }
+
+  render() {
     return (
-        <div>
-            <h1> 1</h1>
-        </div>
+      <>
+        {this.state.pokemons ? ( 
+          <div className="pokemonCard"> 
+          { this.state.pokemons.map(poke => 
+          <PokemonCard 
+            key={poke.name}
+            name={poke.name}
+            url={poke.url}
+          />
+      )}
+      </div>
+      ) : (
+      <h1> Loading </h1>
+    )}
+      </>
     )
+  }
 }
 
-export default FirstGenList

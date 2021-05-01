2f6865952e2c5e54402371b6e964e3751309d0a3
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-01 23:55:08 +0100

    Update PokemonCard.js

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,55 +1,66 @@
-import React , { Component } from 'react'
+import React, { Component } from "react";
 import "../assets/css/card.css";
 import { Redirect } from "react-router";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 class PokemonCard extends Component {
-    state = {
-        name: '',
-        image: '',
-        index: '',
-        redirect: false
-    }
+  state = {
+    name: "",
+    image: "",
+    index: "",
+    redirect: false,
+  };
 
-    componentDidMount(){
-        const { name , url } = this.props;
-        const index = url.split('/')[url.split('/').length - 2];
-        const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
+  componentDidMount() {
+    const { name, url } = this.props;
+    const index = url.split("/")[url.split("/").length - 2];
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
 
-        this.setState({
-            name,
-            image,
-            index
-        })
-    }
+    this.setState({
+      name,
+      image,
+      index,
+    });
+  }
 
-    setRedirect = (index) => {
-        this.setState({
-          redirect: true,
-          index: index
-        });
-      };
-      renderRedirect = () => {
-        if (this.state.redirect) {
-          return <Redirect to={{
+  setRedirect = (index) => {
+    this.setState({
+      redirect: true,
+      index: index,
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return (
+        <Redirect
+          to={{
             pathname: `/pokemon/${this.state.index}`,
-           }}/>;
-        }
-      };
-
-    render (){
-        return (
-            <div className="card-item">
-                {this.renderRedirect()}
-                <div>
-                    <p className="cardIndex">{this.state.index}</p>
-                    <p className="cardName">{this.state.name}</p>
-                </div>
-                <img src={this.state.image} alt="pokemon Image" />
-                <Button variant="contained" color="primary" onClick={() => this.setRedirect(`${this.state.index}`)}>Mostrar más</Button>
-            </div>
-        )
+          }}
+        />
+      );
     }
+  };
+
+  render() {
+    return (
+      <div className="card-item">
+        {this.renderRedirect()}
+        <div>
+          <p className="cardIndex">{this.state.index}</p>
+          <p className="cardName">{this.state.name}</p>
+        </div>
+        <img src={this.state.image} alt="pokemon Image" />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.setRedirect(`${this.state.index}`)}
+        >
+          Mostrar más
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default PokemonCard
+export default PokemonCard;

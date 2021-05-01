0d8c8d80cb7b026517fd2efb30b582c0110d7a12
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-01 23:55:10 +0100

    Update searchBar.js

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,34 +1,57 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
 import { Redirect } from "react-router";
 
 export default class SearchBar extends React.Component {
-    
-    state = {
-        value: '',
-        redirect: false
-    }
-
-    setRedirect = (value) => {
-        this.setState({
-          redirect: true,
-          value: value
-        });
-      };
-      renderRedirect = () => {
-        if (this.state.redirect) {
-          return <Redirect to={{
-            pathname: `/pokemon/filtered/${this.state.value}`,
-           }}/>;
-        }
-      };
-
-    render() {
-      return (
-        <form>
-          <input type="text" value={this.state.value} onChange={() => this.setRedirect(`${this.state.value}`)} />
-          <Button variant="contained" color="primary" onChange={() => this.setRedirect(`${this.state.value}`)}>Mostrar más</Button>
-        </form>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = { redirect: false, value: "" };
+    this.setRedirect = this.setRedirect.bind(this);
+    this.fasdf = this.fasdf.bind(this);
   }
+
+  setRedirect(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  fasdf() {
+    const redirect = this.state.redirect;
+
+    if (redirect)
+      this.setState({
+        redirect: false,
+      });
+
+    return (
+      redirect && (
+        <Redirect
+          to={{
+            pathname: `/pokemons/${this.state.value}`,
+          }}
+        />
+      )
+    );
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          this.setState({
+            redirect: true,
+          });
+        }}
+      >
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.setRedirect}
+        />
+
+        {this.fasdf()}
+      </form>
+    );
+  }
+}

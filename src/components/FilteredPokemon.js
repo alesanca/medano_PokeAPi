d417be7efff5b9d132f4d59e892ff26f39b3f979
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-01 23:55:05 +0100

    Update FilteredPokemon.js

--- src/components/FilteredPokemon.js
+++ src/components/FilteredPokemon.js
@@ -1,48 +1,35 @@
-import axios from 'axios';
-import React from 'react'
+import axios from "axios";
+import React from "react";
 import PokemonCard from "./PokemonCard";
 
 export default class FilteredPokemon extends React.Component {
+  state = {
+    url: "https://pokeapi.co/api/v2/pokemon?limit=50",
+    pokemons: [],
+  };
 
-    state = {
-        url :'https://pokeapi.co/api/v2/pokemon?limit=600',
-        pokemons: null
-      }
-    
-      async componentDidMount() {
+  async componentDidMount() {
+    const { value } = this.props.match.params;
+    const res = await axios.get(this.state.url);
+    const noFiltered = res.data.results;
+    const filtered = noFiltered.filter((poke) => poke.name.startsWith(value));
+    console.log({ value, noFiltered, filtered });
+    this.setState({ pokemons: filtered });
+  }
 
-        const { value } = this.props.match.params;
-
-        const res = await axios.get(this.state.url);
-        this.setState({ pokemons : res.data['results']})
-
-        const noFiltered = this.state.pokemons;
-        const filtered = [];
-
-        for(let i = 0 ; i < this.state.pokemons ; i++){
-            if(this.state.pokemons[i].name .startsWith({value})){
-                console.log("It works");
-            }
-        }
-      }
-    
-      render() {
-        return (
-          <>
-            {this.state.pokemons ? ( 
-              <div className="pokemonCard"> 
-              { this.state.pokemons.map(poke => 
-              <PokemonCard 
-                key={poke.name}
-                name={poke.name}
-                url={poke.url}
-              />
-          )}
+  render() {
+    return (
+      <>
+        {this.state.pokemons.length ? (
+          <div className="pokemonCard">
+            {this.state.pokemons.map((poke) => (
+              <PokemonCard key={poke.name} name={poke.name} url={poke.url} />
+            ))}
           </div>
-          ) : (
+        ) : (
           <h1> Loading </h1>
         )}
-          </>
-        )
-      }
+      </>
+    );
+  }
 }

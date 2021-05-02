7eca9d0f1cdd7521cbcdec4627d1dcca0fcdefa8
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-02 19:19:48 +0100

    Update Home.js
    Changed classes to hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import PokemonCard from "./PokemonCard";
 
-export default class Home extends React.Component {
-  state = {
+const Home = () => {
+  const [currentState, setNewState] = useState({
     url: "https://pokeapi.co/api/v2/pokemon?limit=600",
     pokemons: null,
-  };
+  });
 
-  async componentDidMount() {
-    const res = await axios.get(this.state.url);
-    this.setState({ pokemons: res.data["results"] });
-  }
+  useEffect(() => {
+    getApi();
+    async function getApi() {
+      const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=600"); //I tried to use currentState.url but I got an error
+      setNewState({ pokemons: res.data["results"] });
+    }
+  }, []);
 
-  render() {
-    return (
-      <>
-        {this.state.pokemons ? (
-          <div className="pokemonCards">
-            {this.state.pokemons.map((poke) => (
-              <PokemonCard key={poke.name} name={poke.name} url={poke.url} />
-            ))}
-          </div>
-        ) : (
-          <h1> Loading </h1>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {currentState.pokemons ? (
+        <div className="pokemonCards">
+          {currentState.pokemons.map((poke) => (
+            <PokemonCard key={poke.name} name={poke.name} url={poke.url} />
+          ))}
+        </div>
+      ) : (
+        <h1> Loading </h1>
+      )}
+    </>
+  );
+};
+
+export default Home;

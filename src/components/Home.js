f7a2fd204637cace1b5ccd2f242bdbc0b8cc093f
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-02 11:27:40 +0100

    Update Home.js
    Prettier applied

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,32 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 import PokemonCard from "./PokemonCard";
 
 export default class Home extends React.Component {
   state = {
-    url :'https://pokeapi.co/api/v2/pokemon?limit=600',
-    pokemons: null
-  }
+    url: "https://pokeapi.co/api/v2/pokemon?limit=600",
+    pokemons: null,
+  };
 
   async componentDidMount() {
     const res = await axios.get(this.state.url);
-    this.setState({ pokemons : res.data['results']})
+    this.setState({ pokemons: res.data["results"] });
   }
 
   render() {
     return (
       <>
-        {this.state.pokemons ? ( 
-          <div className="pokemonCard"> 
-          { this.state.pokemons.map(poke => 
-          <PokemonCard 
-            key={poke.name}
-            name={poke.name}
-            url={poke.url}
-          />
-      )}
-      </div>
-      ) : (
-      <h1> Loading </h1>
-    )}
+        {this.state.pokemons ? (
+          <div className="pokemonCards">
+            {this.state.pokemons.map((poke) => (
+              <PokemonCard key={poke.name} name={poke.name} url={poke.url} />
+            ))}
+          </div>
+        ) : (
+          <h1> Loading </h1>
+        )}
       </>
-    )
+    );
   }
 }

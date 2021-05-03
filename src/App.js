6df7a78cd2fde9877007646f7524a4c92bc5d134
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-03 10:40:45 +0100

    Update App.js
    Prettier applied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-//Import de componentes y utilidades
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Nav, NavLink, Bars, NavMenu } from "./NavBarElements";
-import Logo from "./assets/images/logo.png"
+import Logo from "./assets/images/logo.png";
 
-//Import de hojas de estilos
 import "./assets/css/navbar.css";
 
-//Import de las diferentes páginas de navegación
 import Home from "./components/Home";
 import FirstGenList from "./components/FirstGenList";
 import SecondGenList from "./components/SecondGenList";
@@ -27,41 +24,29 @@ function App() {
             </NavLink>
             <Bars />
             <NavMenu>
-              <NavLink
-                to="/1gen"
-                className="navBarItem"
-                activeStyle
-              >
+              <NavLink to="/1gen" className="navBarItem" activeStyle>
                 1º Gen
               </NavLink>
-              <NavLink
-                to="/2gen"
-                className="navBarItem"
-                activeStyle
-              >
+              <NavLink to="/2gen" className="navBarItem" activeStyle>
                 2º Gen
               </NavLink>
               <NavLink to="/3gen" className="navBarItem" activeStyle>
                 3º Gen
               </NavLink>
-              <SearchBar to="/pokemons/:value"/>
+              <SearchBar to="/pokemons/:value" />
             </NavMenu>
           </Nav>
         </div>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/1gen" component={FirstGenList} />
-          <Route
-            path="/2gen"
-            component={SecondGenList}
-          />
+          <Route path="/2gen" component={SecondGenList} />
           <Route path="/3gen" component={ThirdGenList} />
-          <Route path="/pokemon/:index" component={SinglePokemon}/>
-          <Route path="/pokemons/:value" component={FilteredPokemon}/>
+          <Route path="/pokemon/:index" component={SinglePokemon} />
+          <Route path="/pokemons/:value" component={FilteredPokemon} />
         </Switch>
       </Router>
       <Footer />
-   
     </>
   );
 }

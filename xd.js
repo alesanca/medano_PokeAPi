af33d55f90f5933641dec3ce02d2dedc2975e14f
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-05-03 10:37:03 +0100

    Test file - ignore
    Test file

--- xd.js
+++ xd.js
@@ -37,3 +37,78 @@ const FilteredPokemon = () => {
 };
 
 export default FilteredPokemon;
+
+import React, { useState, useEffect } from "react";
+import "../assets/css/card.css";
+import { Redirect } from "react-router";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const PokemonCard = (props) => {
+  const [currentPokemon, setPokemon] = useState({
+    name: "",
+    image: "",
+    index: "",
+  });
+
+  const [redirect, setNewRedirect] = useState(false);
+
+  useEffect(() => {
+    getApi();
+    function getApi() {
+      const { name, url } = props;
+      const index = url.split("/")[url.split("/").length - 2];
+      const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
+
+      setPokemon({
+        name,
+        image,
+        index,
+      });
+    }
+  });
+
+  const setRedirect = (index) => {
+    setPokemon({ index: index });
+    setNewRedirect(true);
+  };
+
+  const renderRedirect = () => {
+    if (redirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: `/pokemon/${currentPokemon.index}`,
+          }}
+        />
+      );
+    }
+  };
+
+  return (
+    <Card>
+      {renderRedirect()}
+      <CardContent>
+        <Typography color="textPrimary" gutterBottom>
+          {currentPokemon.index}
+        </Typography>
+        <Typography color="textSecondary" gutterBottom>
+          {currentPokemon.name}
+        </Typography>
+        <img src={currentPokemon.image} alt="pokemon Image" />
+      </CardContent>
+      <Button
+        classname="buttonCard"
+        variant="contained"
+        color="primary"
+        onClick={() => setRedirect(`${currentPokemon.index}`)}
+      >
+        Mostrar más
+      </Button>
+    </Card>
+  );
+};
+
+export default PokemonCard;
